be63d121f08bd4fe6f3a102faf5e802bffbcd943
qwhuxs <vtk241_kai@student.ztu.edu.ua>
2025-10-25 01:54:20 +0300

    Оновлено App.js: додано стан score і передачу фінального результату між сторінками гри

--- src/App.js
+++ src/App.js
@@ -4,18 +4,57 @@ import Header from './components/Header';
 import StartPage from './pages/StartPage';
 import GamePage from './pages/GamePage';
 import ResultPage from './pages/ResultPage';
+import { useGameLogic } from './hooks/useGameLogic';
 
 function App() {
   const [page, setPage] = useState('start');
+  const [finalScore, setFinalScore] = useState(0);
+
+  const {
+    round,
+    score,
+    scrambledWord,
+    userInput,
+    setUserInput,
+    checkAnswer,
+    isFinished,
+    restartGame,
+  } = useGameLogic();
+
+  const handleStart = () => setPage('game');
+
+  const handleFinish = (scoreFromGame) => {
+    setFinalScore(scoreFromGame);
+    setPage('result');
+  };
+
+  const handleRestart = () => {
+    restartGame();
+    setPage('start');
+  };
 
   const renderPage = () => {
     switch (page) {
       case 'start':
-        return <StartPage onStart={() => setPage('game')} />;
+        return <StartPage onStart={handleStart} />;
+
       case 'game':
-        return <GamePage onFinish={() => setPage('result')} />;
+        if (isFinished) handleFinish(score);
+        return (
+          <GamePage
+            round={round}
+            score={score}
+            scrambledWord={scrambledWord}
+            userInput={userInput}
+            setUserInput={setUserInput}
+            checkAnswer={checkAnswer}
+            onFinish={() => handleFinish(score)}
+          />
+        );
+
       case 'result':
-        return <ResultPage onRestart={() => setPage('start')} />;
+        return <ResultPage score={finalScore} onRestart={handleRestart} />;
+
       default:
         return null;
     }
